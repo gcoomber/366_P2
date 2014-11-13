efbe9427d9b204ef3d7dda34aa0068f3ffb5326c
George Coomber <georgecoomber@gmail.com>
2014-11-13 15:17:04 -0700

    starting part 1

--- Tilecoder.py
+++ Tilecoder.py
@@ -1,13 +1,20 @@
 numTilings = 8
-    
+tileSize = 0.6
+tileOffset = 1 / numTilings
+oneDimensionTileCount = 10
+
 def tilecode(in1,in2,tileIndices):
-    " write your tilecoder here (5 lines or so)"
-    
+	# Calculate the indicies in each of the tilings
+	for i in range(numTilings):
+		    
     
 def printTileCoderIndices(in1,in2):
     tileIndices = [-1]*numTilings
     tilecode(in1,in2,tileIndices)
     print('Tile indices for input (',in1,',',in2,') are : ', tileIndices)
+
+def getTranslatedCoordinated(in1, in2, tileIndex):
+	index1 = in1 + (tileIndex * tileSize) / 
 
 printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
