ef8e864d685d42946f5aaed5849a33ff72604427
dwaldon <dwaldon@ualberta.ca>
2014-11-13 23:13:55 -0700

    Finished tilecode function
    Also commented out the code throwing errors in getTranslatedCoordinated,
    and added the pass. Probably can delete this whole method... it was one
    line of code, so I put it inline into tilecode (lines 10 and 11)

--- Tilecoder.py
+++ Tilecoder.py
@@ -5,16 +5,28 @@ oneDimensionTileCount = 10
 
 def tilecode(in1,in2,tileIndices):
 	# Calculate the indicies in each of the tilings
-	for i in range(numTilings):
-		    
+	for tilingIndex in range(numTilings):
+		# get local tile coordinates 
+		tileX = int( (in1 + (tilingIndex*tileSize)/numTilings) / 0.6)
+		tileY = int( (in2 + (tilingIndex*tileSize)/numTilings) / 0.6)
+
+		# get the local tile number (between 0 and oneDimensionTileCount^2)
+		localTileNumber = tileX + tileY*(oneDimensionTileCount+1)
+
+		# get the global tile number and populate the tileIndices vector
+		globalTileNumber = tilingIndex*(pow(oneDimensionTileCount+1,2)) + localTileNumber
+		tileIndices[tilingIndex] = globalTileNumber
+
     
 def printTileCoderIndices(in1,in2):
     tileIndices = [-1]*numTilings
     tilecode(in1,in2,tileIndices)
     print('Tile indices for input (',in1,',',in2,') are : ', tileIndices)
 
-def getTranslatedCoordinated(in1, in2, tileIndex):
-	index1 = in1 + (tileIndex * tileSize) / 
+def getTranslatedCoordinated(in1, in2, tilingIndex):
+	# index1 = in1 + (tilingIndex * tileSize) / 
+	pass
+
 
 printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
